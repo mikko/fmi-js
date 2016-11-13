9416aa54e6c83d468c3e406a21e90d096a2fdb0b
Mikko Pohja <mikko.pohja@futurice.com>
2016-11-13 23:03:38 +0200

    Parser and document generator for fmi stored queries

--- main.js
+++ main.js
@@ -1,22 +1,7 @@
-const config = require('./config');
 const listStoredQueries = require('./query/listStoredQueries');
-const simpleForecast = require('./query/simpleForecast');
-const latestObservations = require('./query/latestObservations');
 
-//console.log(listStoredQueries.run(config.apiKey));
-
-const testLocation = 'suinula';
-
-simpleForecast.run(testLocation)
+listStoredQueries.run()
   .then((res) => {
-    console.log(`Simple forecast for ${testLocation}`);
-    console.log(res);
-  })
-  .catch(err => console.log(err));
+    listStoredQueries.renderMarkdown(res);
+  });
 
-latestObservations.run(testLocation)
-  .then((res) => {
-    console.log(`Latest observations for ${testLocation}`);
-    console.log(res);
-  })
-  .catch(err => console.log(err));
--- query/listStoredQueries.js
+++ query/listStoredQueries.js
@@ -1,10 +1,57 @@
+const fs = require('fs');
 const fmiRequest = require('../lib/fmiRequest');
 
 const queryId = 'describeStoredQueries';
 exports.queryId = queryId;
 
+const xmlFirstLevel = 'DescribeStoredQueriesResponse';
+const xmlSecondLevel = 'StoredQueryDescription';
+
 exports.run = function runQuery() {
-  return fmiRequest.get(queryId)
-    .then(results => Promise.resolve(results))
-    .catch(err => console.log(err));
+  return fmiRequest.rawGet({ path: `request=${queryId}` })
+    .then((results) => {
+      const storedQueries = results[xmlFirstLevel][xmlSecondLevel].map((xmlDesc) => {
+        const rawAbstract = xmlDesc.Abstract[0];
+        const abstract = typeof rawAbstract === 'string' ? rawAbstract.replace(/(\r\n|\n|\r)/gm, '').trim() : '';
+        console.log(typeof rawAbstract, JSON.stringify(rawAbstract, null, 2));
+        return {
+          title: xmlDesc.Title,
+          queryId: xmlDesc.$.id,
+          abstract,
+          parameters: xmlDesc.Parameter.map((param) => {
+            console.dir(param);
+            const rawParamAbstract = param.Abstract[0];
+            const paramAbstract = typeof rawParamAbstract === 'string' ? rawParamAbstract.replace(/(\r\n|\n|\r)/gm, '').trim() : '';
+            return {
+              name: param.$.name,
+              type: param.$.type,
+              title: param.Title,
+              abstract: paramAbstract,
+            };
+          }),
+        };
+      });
+      return Promise.resolve(storedQueries);
+    })
+    .catch(err => console.log('Error in getting storedQueries', err));
+};
+
+exports.renderMarkdown = (doc) => {
+  const md = doc.map(storedQuery => (
+    `
+
+## ${storedQuery.title}
+
+__${storedQuery.queryId}__
+
+${storedQuery.abstract}
+### Parameters
+
+| Name | Type | Title | Abstract |
+| --- | --- | --- | --- |
+| ${storedQuery.parameters.map(param => `${param.name} | ${param.type} | ${param.title} | ${param.abstract}`).join(' \n ')} |
+
+
+`)).join('\n');
+  fs.writeFileSync('storedQueries.md', `# Stored queries\n${md}`);
 };
