ae94b8ee77c9e4197d33e377476d610444872e12
Mikko Pohja <mikko.pohja@futurice.com>
2016-11-13 23:03:09 +0200

    Destructured parameters for fmi request

--- query/simpleForecast.js
+++ query/simpleForecast.js
@@ -5,7 +5,7 @@ exports.queryId = queryId;
 exports.params = ['place'];
 
 exports.run = function runQuery(place) {
-  return fmiRequest.get(queryId, { place })
+  return fmiRequest.get({ queryId, params: { place } })
     .then(results => Promise.resolve(results))
     .catch(err => console.log(err));
 };
