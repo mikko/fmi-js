34455930677c739f1445b07d4e16a4444d833bf4
Hannu-Pekka Heinäjärvi <hannu-pekka.heinajarvi@futurice.com>
2017-03-30 08:55:49 +0300

    Add function to get forecasts by latitude and longitude.

--- query/simpleForecast.js
+++ query/simpleForecast.js
@@ -2,10 +2,19 @@ const fmiRequest = require('../lib/fmiRequest');
 
 const queryId = 'fmi::forecast::hirlam::surface::point::simple';
 exports.queryId = queryId;
-exports.params = ['place'];
+exports.params = ['place', 'latlng'];
 
 exports.run = function runQuery(place) {
   return fmiRequest.get({ queryId, params: { place } })
+    .then(results => Promise.resolve(results))
+    .catch(err => console.log(err));
+};
+
+/**
+ * Fetches forecasts by LatLng, eg. 61.23,23.31
+ */
+exports.runLatLng = function runQuery(latlon) {
+  return fmiRequest.get({ queryId, params: { latlon } })
     .then(results => Promise.resolve(results))
     .catch(err => console.log(err));
 };
