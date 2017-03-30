3c9da81b176dd1b9706e6f7a397caeb7c3a84c98
Hannu-Pekka Heinäjärvi <hannu-pekka.heinajarvi@futurice.com>
2017-03-30 08:58:51 +0300

    Fix param name.

--- query/simpleForecast.js
+++ query/simpleForecast.js
@@ -2,7 +2,7 @@ const fmiRequest = require('../lib/fmiRequest');
 
 const queryId = 'fmi::forecast::hirlam::surface::point::simple';
 exports.queryId = queryId;
-exports.params = ['place', 'latlng'];
+exports.params = ['place', 'latlon'];
 
 exports.run = function runQuery(place) {
   return fmiRequest.get({ queryId, params: { place } })
@@ -13,7 +13,7 @@ exports.run = function runQuery(place) {
 /**
  * Fetches forecasts by LatLng, eg. 61.23,23.31
  */
-exports.runLatLng = function runQuery(latlon) {
+exports.runLatLon = function runQuery(latlon) {
   return fmiRequest.get({ queryId, params: { latlon } })
     .then(results => Promise.resolve(results))
     .catch(err => console.log(err));
